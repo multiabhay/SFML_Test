aa32b69ab97243a01f88d3ff393eb99659bb1f21
multiabhay <multiabhay@gmail.com>
2017-07-18 11:10:26 +0530

    test.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,45 @@
-#include <iostream>
+
+
+#include<iostream>
+#include<SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "wht the hell " << std::endl;
-    
-  return 0;  
-}
 
+  sf::RenderWindow window(sf::VideoMode(1280,720), "What the hell");
+  sf::Event e;
+
+  // a circle
+  sf::CircleShape circle;
+  circle.setRadius(30);
+  circle.setOrigin(30,30);
+  circle.setFillColor(sf::Color::Red);
+  circle.setPosition(640,380);
+  //a rect
+  sf::RectangleShape rect(sf::Vector2f(200,10));
+  rect.setFillColor(sf::Color::White);
+  rect.setOrigin(rect.getSize() * 0.5f);
+  rect.setPosition(640,380);
+
+  while(window.isOpen())
+  {
+    while(window.pollEvent(e))
+    {
+      if(e.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+    }
+
+    //update
+    rect.rotate(0.1);
+
+    window.clear();
+    window.draw(circle);
+    window.draw(rect);
+    window.display();
+  }
+
+  return EXIT_SUCCESS;
+
+}
